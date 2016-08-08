88a40326695abfd6a2ba60adc3a487d9ac959d14
Jeremias Yehdegho <j.yehdegho@gmail.com>
2016-08-08 16:27:54 +0200

    add messagebuffer

---
+++ mondrian/messagebuffer.go
@@ -0,0 +1,109 @@
+package mondrian
+
+import (
+	"github.com/nsf/termbox-go"
+)
+
+// Message is an entry for a MessageBuffer.
+type Message interface {
+	Draw(*Region)
+	Height(int) int
+}
+
+// MessageBuffer is an interactive widget showing a scrollable list of messages.
+type MessageBuffer struct {
+	*Region
+	Visible
+
+	messages    []Message
+	totalHeight int
+	scrollPos   int
+	follow      bool
+}
+
+// NewMessageBuffer returns a MessageBuffer.
+func NewMessageBuffer() *MessageBuffer {
+	return &MessageBuffer{
+		Region:   defaultRegion,
+		messages: []Message{},
+		follow:   true,
+	}
+}
+
+// Resize resizes the MessageBuffer.
+func (m *MessageBuffer) Resize(r *Region) {
+	m.Region = r
+	m.totalHeight = 0
+	for _, msg := range m.messages {
+		m.totalHeight += msg.Height(r.Width)
+	}
+}
+
+// Draw draws the MessageBuffer.
+func (m *MessageBuffer) Draw() {
+	m.Move(0, 0)
+	m.Clear()
+
+	// scrolling beyond content/follow mode
+	if m.scrollPos > m.totalHeight-m.Height || m.follow {
+		m.scrollPos = m.totalHeight - m.Height
+		m.follow = true
+	}
+	// if there are not enough lines to fill the screen, start at the top and
+	// possibly leave some empty space.
+	if m.scrollPos < 0 {
+		m.scrollPos = 0
+	}
+
+	// find first message to draw
+	m.Cy = -m.scrollPos
+	startIndex := 0
+	for m.Cy < 0 && len(m.messages[startIndex:]) > 0 {
+		// found
+		if m.Cy+m.messages[startIndex].Height(m.Width) > 0 {
+			break
+		}
+		m.Cy += m.messages[startIndex].Height(m.Width)
+		startIndex++
+	}
+
+	// draw
+	for _, msg := range m.messages[startIndex:] {
+		// outside of region, no need to draw anymore
+		if m.Cy > m.Height {
+			break
+		}
+		msg.Draw(m.Region)
+		m.Cx = 0
+		m.Cy++
+	}
+}
+
+// HandleKey responds to user input.
+func (m *MessageBuffer) HandleKey(ev termbox.Event) {
+	redraw := true
+	switch ev.Key {
+	case termbox.KeyPgup:
+		m.scrollPos -= m.Height / 2
+		m.follow = false
+	case termbox.KeyPgdn:
+		m.scrollPos += m.Height / 2
+		m.follow = false
+	case termbox.KeyHome:
+		m.scrollPos = 0
+		m.follow = false
+	case termbox.KeyEnd:
+		m.follow = true
+	default:
+		redraw = false
+	}
+	if redraw {
+		Draw(m)
+	}
+}
+
+// Append appends a message.
+func (m *MessageBuffer) Append(msg Message) {
+	m.messages = append(m.messages, msg)
+	Draw(m)
+}
---
+++ mondrian/messagebuffer_test.go
@@ -0,0 +1,45 @@
+package mondrian
+
+import (
+	"testing"
+)
+
+type mockMsg struct {
+	s string
+}
+
+func (mm *mockMsg) Draw(r *Region) {
+	r.Printf("%s", mm.s)
+}
+
+func (mm *mockMsg) Height(width int) int {
+	r := &Region{X: 1000, Y: 1000, Width: width, Height: 1000}
+	mm.Draw(r)
+	return r.Cy + 1
+}
+
+func TestMessageBufferDraw(t *testing.T) {
+	SetMockUI(20, 100)
+	m := NewMessageBuffer()
+	m.SetVisibility(true)
+	m.Resize(&Region{Width: 20, Height: 100})
+
+	s0 := "this is a test"
+	s1 := "123456789 123456789 next line"
+	m.Append(&mockMsg{s0})
+	m.Append(&mockMsg{s1})
+
+	Draw(m)
+
+	for k, ch := range s0 {
+		if ch != mockBuffer[k][0] {
+			t.Errorf("mockBuffer[%d][0] = %c != %c", k, mockBuffer[k][0], ch)
+		}
+	}
+
+	for k, ch := range "next line" {
+		if ch != mockBuffer[k][2] {
+			t.Errorf("mockBuffer[%d][2] = %c != %c", k, mockBuffer[k][2], ch)
+		}
+	}
+}
