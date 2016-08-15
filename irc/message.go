8494869b055796de050c8f6c0ddec448e8389fc4
Jeremias Yehdegho <j.yehdegho@gmail.com>
2016-08-15 13:51:48 +0200

    add irc message parsing

---
+++ irc/message.go
@@ -0,0 +1,50 @@
+package irc
+
+import (
+	"errors"
+	"strings"
+	"time"
+)
+
+type message struct {
+	Prefix   string
+	Command  string
+	Params   []string
+	Trailing string
+	Raw      string
+	ToA      time.Time
+	From     string
+}
+
+var (
+	malformed error = errors.New("Malformed message")
+)
+
+// See: http://calebdelnay.com/blog/2010/11/parsing-the-irc-message-format-as-a-client
+func parse(raw string) (*message, error) {
+	ret := &message{Raw: raw, ToA: time.Now()}
+
+	prefixEnd := -1
+	if strings.HasPrefix(raw, ":") {
+		prefixEnd = strings.Index(raw, " ")
+		if prefixEnd < 0 {
+			return nil, malformed
+		}
+		ret.Prefix = raw[1:prefixEnd]
+	}
+
+	trailingStart := strings.Index(raw, " :")
+	if trailingStart >= 0 {
+		ret.Trailing = raw[trailingStart+2:]
+	} else {
+		trailingStart = len(raw)
+	}
+
+	cmdparams := strings.Split(raw[prefixEnd+1:trailingStart], " ")
+	if len(cmdparams) > 0 {
+		ret.Command, ret.Params = cmdparams[0], cmdparams[1:]
+	} else {
+		return nil, malformed
+	}
+	return ret, nil
+}
---
+++ irc/message_test.go
@@ -0,0 +1,43 @@
+package irc
+
+import (
+	"testing"
+)
+
+func compare(a, b *message) bool {
+	if len(a.Params) != len(b.Params) {
+		return false
+	}
+	ret := a.Prefix == b.Prefix && a.Command == b.Command && a.Trailing == b.Trailing
+	for k := range a.Params {
+		ret = ret && a.Params[k] == b.Params[k]
+	}
+	return ret
+}
+
+func TestParse(t *testing.T) {
+	in := []string{
+		":Kevin!bncworld@I-Have.a.cool.vhost.com PRIVMSG #mIRC :I feel lucky today",
+		"PING :something",
+		":CalebDelnay!calebd@localhost MODE #mychannel -l",
+		":CalebDelnay!calebd@localhost QUIT :Bye bye!",
+		":Macha!~macha@unaffiliated/macha PRIVMSG #botwar :Test response",
+	}
+	out := []*message{
+		{Prefix: "Kevin!bncworld@I-Have.a.cool.vhost.com", Command: "PRIVMSG", Params: []string{"#mIRC"}, Trailing: "I feel lucky today"},
+		{Prefix: "", Command: "PING", Params: []string{}, Trailing: "something"},
+		{Prefix: "CalebDelnay!calebd@localhost", Command: "MODE", Params: []string{"#mychannel", "-l"}, Trailing: ""},
+		{Prefix: "CalebDelnay!calebd@localhost", Command: "QUIT", Params: []string{}, Trailing: "Bye bye!"},
+		{Prefix: "Macha!~macha@unaffiliated/macha", Command: "PRIVMSG", Params: []string{"#botwar"}, Trailing: "Test response"},
+	}
+
+	for k := range in {
+		p, err := parse(in[k])
+		if err != nil {
+			t.Error(err)
+		}
+		if !compare(p, out[k]) {
+			t.Errorf("%+v != %+v\n", p, out[k])
+		}
+	}
+}
