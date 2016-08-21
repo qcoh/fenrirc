9ce951cfc279c00d8199282fc9354a5cf47b6582
Jeremias Yehdegho <j.yehdegho@gmail.com>
2016-08-21 18:25:52 +0200

    add commands and parser

---
+++ command.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"errors"
+	"strings"
+)
+
+var (
+	errMalformed = errors.New("Malformed input")
+)
+
+// Command is a command from the user (prompt).
+type Command struct {
+	// TODO: Name this better
+	Command string
+	Params  []string
+	Raw     string
+}
+
+func parse(raw string) (*Command, error) {
+	if len(raw) == 0 {
+		return nil, errMalformed
+	}
+
+	ret := &Command{Raw: raw}
+
+	if raw[0] != '/' {
+		return ret, nil
+	}
+
+	s := strings.Split(raw[1:], " ")
+	if len(s) <= 1 {
+		ret.Command = s[0]
+	} else {
+		ret.Command, ret.Params = s[0], s[1:]
+	}
+	return ret, nil
+}
