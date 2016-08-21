be2d4ea9e8f1e0a6e449ff64925cef7e6fe9db52
Jeremias Yehdegho <j.yehdegho@gmail.com>
2016-08-21 18:47:35 +0200

    more handlers

--- handler.go
+++ handler.go
@@ -1,6 +1,45 @@
 package main
 
+import (
+	"fenrirc/irc"
+)
+
 // A Handler is the interface implemented by everything reacting to user (prompt) input.
 type Handler interface {
 	Handle(*Command)
+}
+
+// ServerHandler sends user input to the server.
+type ServerHandler struct {
+	client *irc.Client
+	next   Handler
+}
+
+// Handle reacts to a command.
+func (sh *ServerHandler) Handle(cmd *Command) {
+	switch cmd.Command {
+	case "WHOIS":
+	default:
+		if sh.next != nil {
+			sh.next.Handle(cmd)
+		}
+	}
+}
+
+// ChannelHandler sends user input relevant to a channel to the server.
+type ChannelHandler struct {
+	client *irc.Client
+	name   string
+	next   Handler
+}
+
+// Handle reacts to a command.
+func (ch *ChannelHandler) Handle(cmd *Command) {
+	switch cmd.Command {
+
+	default:
+		if ch.next != nil {
+			ch.next.Handle(cmd)
+		}
+	}
 }
