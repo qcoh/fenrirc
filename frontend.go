791ab1597f211eff63919970659d2a272f359596
Jeremias Yehdegho <j.yehdegho@gmail.com>
2016-08-31 19:04:50 +0200

    accessor methods for frontend

--- frontend.go
+++ frontend.go
@@ -15,7 +15,8 @@ type namedView struct {
 
 // Frontend comprises the server and channel windows of an IRC connection.
 type Frontend struct {
-	views []*namedView
+	views  []*namedView
+	vindex int
 }
 
 // NewFrontend constructs a Frontend.
@@ -37,4 +38,30 @@ func (f *Frontend) NewChannel(name string) irc.Appender {
 	}
 	f.views = append(f.views, &namedView{view: NewMessageBuffer(), name: name})
 	return f.views[len(f.views)-1].view
+}
+
+func (f *Frontend) next() mondrian.InteractiveWidget {
+	if f.vindex+1 < len(f.views) {
+		f.vindex++
+		return f.views[f.vindex].view
+	}
+	return nil
+}
+
+func (f *Frontend) prev() mondrian.InteractiveWidget {
+	if f.vindex > 0 {
+		f.vindex--
+		return f.views[f.vindex].view
+	}
+	return nil
+}
+
+func (f *Frontend) first() mondrian.InteractiveWidget {
+	f.vindex = 0
+	return f.views[f.vindex].view
+}
+
+func (f *Frontend) last() mondrian.InteractiveWidget {
+	f.vindex = len(f.views) - 1
+	return f.views[f.vindex].view
 }
